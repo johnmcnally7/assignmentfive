8080e9f717088a1ef997b5116f604663b0a0cc2a
johnmcnally7 <johnmcnally7@John-McNallys-MacBook-Pro.local>
2017-03-06 20:54:56 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+    //create map
+    var map = L.map('mapcontainer', {
+        center: [40.6622, -73.9442],
+        zoom: 12
+    });
+
+    //tile layer
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+      }).addTo(map);
+
+
+    //points
+    var info = L.geoJson(wifiData, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                color: '#7FFFD4',
+                radius: 0.1,
+                weight: 3
+
+            })
+        }
+    }).addTo(map);
+
+
+    //lighter outer glow
+    var glow = L.geoJson(wifiData, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                color: '#7FFFD4',
+                radius: 4,
+                opacity: 0.1
+            })
+        }
+    }).addTo(map);
+
+ //scale bar 
+L.control.scale({
+    metric: false,
+    imperial: true,
+    maxWidth:200
+}).addTo(map);
+
