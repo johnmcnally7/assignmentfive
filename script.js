91866d68940457970216ea69649e79cc65d7cd65
johnmcnally7 <johnmcnally7@172-16-176-38.DYNAPOOL.NYU.EDU>
2017-03-09 15:58:33 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-    //create map
-    var map = L.map('mapcontainer', {
+//create map
+ var map = L.map('mapcontainer', {
         center: [40.6622, -73.9442],
         zoom: 12
     });
 
     //tile layer
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
       }).addTo(map);
@@ -17,3 +17,29 @@ L.control.scale({
     maxWidth:200
 }).addTo(map);
 
+//points
+var wifiData = L.geoJson(wifiData, {
+     pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: 'LightSeaGreen',
+            radius: 0.1,
+            weight: 3
+
+         })
+    },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup('<b>Network Name:</b> ' + feature.properties.ssid + '<b><br>Provider:</b> ' + feature.properties.provider)
+    }
+ }).addTo(map);
+
+//lighter outer glow
+var glow = L.geoJson(wifiData, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: 'DodgerBlue',
+            radius: 6,
+            opacity: 0.1
+        })
+     }
+ }).addTo(map);
+    
