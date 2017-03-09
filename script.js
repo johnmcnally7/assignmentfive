19eb1ae7a4144b9185a3c9464dbbf09bab7a073e
johnmcnally7 <johnmcnally7@172-16-176-38.DYNAPOOL.NYU.EDU>
2017-03-09 16:18:04 -0500

    undo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     });
 
     //tile layer
-    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
       }).addTo(map);
